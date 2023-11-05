d9b2b1e11ba010d1dfcb8bd123ae82f18f477eb8
Laurenda Edorh <laurenda98@gmail.com>
2023-11-05 23:30:38 +0100

    section 6 video 15 conditionner un afichage avec gnIf

---
+++ src/app/mock-pokemon-list.ts
@@ -0,0 +1,129 @@
+import { Pokemon } from "./pokemon";
+
+export const POKEMONS: Pokemon[] =
+[
+{
+    id:1,
+    name:"Bulbizzare",
+    hp:25,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:2,
+    name:"Salameche",
+    hp:28,
+    cp:6,
+ 
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:3,
+    name:"Carapuche",
+    hp:21,
+    cp:4,
+ 
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Eau"],
+    created: new Date()
+},
+{
+    id:4,
+    name:"Aspico",
+    hp:16,
+    cp:2,
+  
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:5,
+    name:"Roucoo",
+    hp:25,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Inspecte","poison"],
+    created: new Date()
+},
+{
+    id:6,
+    name:"Rotaa",
+    hp:25,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Normal"],
+    created: new Date()
+},
+{
+    id:7,
+    name:"Bulbizzare",
+    hp:14,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:8,
+    name:"Bulbizzare",
+    hp:25,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:9,
+    name:"plafobec",
+    hp:25,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:10,
+    name:"Abo",
+    hp:25,
+    cp:5,
+   
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:11,
+    name:"Pikatu",
+    hp:25,
+    cp:5,
+ 
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:12,
+    name:"Melophe",
+    hp:25,
+    cp:5,
+  
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+{
+    id:13,
+    name:"Groupix",
+    hp:25,
+    cp:5,
+    picture:"htpps//assets.pokemons.com/asesset/cms2/img/pokedex",
+    types:["Plante","poison"],
+    created: new Date()
+},
+
+];
